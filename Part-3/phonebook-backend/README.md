ee6018f2f3eff32a577e59bd2a5359f82f3c52e0
lahmenderpe <erenler.cihan.github@gmail.com>
2023-09-30 13:20:22 +0300

    build ui

---
+++ Part-3/phonebook-backend/README.md
@@ -0,0 +1,1 @@
+[link to the online application](https://phonebook-backend-6ec4.onrender.com/api/persons)
--- Part-3/phonebook-backend/server.js
+++ Part-3/phonebook-backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 app.use(
