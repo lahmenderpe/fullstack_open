2d587b7eceb817e62b44c9d7dd682dae00573443
lahmenderpe <erenler.cihan.github@gmail.com>
2023-10-04 16:56:49 +0300

    buildui

---
+++ Part-3/phonebook-backend/models/person.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const url = process.env.MONGODB_URL;
+
+mongoose
+	.connect(url)
+	.then((response) => console.log("connected to Mongo"))
+	.catch((error) => console.log("error connecting to MongoDB:", error.message));
+
+const personSchema = mongoose.Schema({
+	name: {
+		type: String,
+		minLength: 3,
+		required: true,
+	},
+	number: {
+		type: Number,
+		required: true,
+		minLength: 8,
+		validate: {
+			validator: function (number) {
+				return /^\d{2,3}-\d+$/.test(number);
+			},
+		},
+	},
+});
+
+personSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+module.exports = Person;
--- Part-3/phonebook-backend/server.js
+++ Part-3/phonebook-backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
+require("dotenv").config();
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.use(express.static("build"));
 app.use(cors());
@@ -44,59 +47,98 @@ let persons = [
 	},
 ];
 
+const errorHandler = (error, request, response, next) => {
+	// console.error(error);
+
+	if (error.name === "CastError") {
+		return response.status(400).send("malformatted id");
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message });
+	}
+
+	next(error);
+};
+
 // Get all persons
 app.get("/api/persons", (req, res) => {
-	res.status(200).send(persons);
+	Person.find({}).then((response) => res.status(200).json(response));
 });
 
 // Get e single person
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
 	const { id } = req.params;
-	const person = persons.find((person) => Number(person.id) === Number(id));
 
-	if (!person) {
-		res.status(404).send();
-		return;
-	}
-
-	res.status(200).json(person);
+	Person.findById(id)
+		.then((response) => {
+			if (response) {
+				res.status(200).json(response);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
 // Delete a single person
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
 	const { id } = req.params;
 
-	persons = persons.filter((person) => Number(person.id) !== Number(id));
-	res.status(200).send();
+	Person.findByIdAndRemove(id)
+		.then((response) => res.status(204).end())
+		.catch((error) => next(error));
 });
 
 // Add a new person
-app.post("/api/persons", (req, res) => {
-	const newId = Math.floor(Math.random() * 100000000);
+app.post("/api/persons", (req, res, next) => {
 	const { name, number } = req.body;
 	const foundName = persons.find(
 		(p) => p.name.toLowerCase() === name.toLowerCase()
 	);
-
 	if (!name || !number) {
 		res.status(400).json({ error: "please provide a name and number" });
 		return;
 	}
-
 	if (foundName) {
 		res.status(400).json({ error: "The name must be unique" });
 		return;
 	}
 
-	const person = { id: newId, ...req.body };
-	persons.push(person);
-	res.status(200).send(person);
+	const person = new Person(req.body);
+
+	person
+		.save()
+		.then((response) => res.status(200).send(response))
+		.catch((error) => next(error));
+});
+
+// Update person
+app.put("/api/persons/:id", (req, res, next) => {
+	const { id } = req.params;
+	const body = req.body;
+
+	Person.findByIdAndUpdate(id, req.body, {
+		new: true,
+		runValidators: true,
+		context: "query",
+	})
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => next(error));
 });
 
 // Get API info
-app.get("/api/info", (req, res) => {
-	const text = `Phonebook has info for ${persons.length} <br> ${new Date()}`;
-	res.status(200).send(text);
+app.get("/api/info", (req, res, next) => {
+	Person.find({})
+		.then((response) => {
+			const text = `Phonebook has info for ${
+				response.length
+			} <br> ${new Date()}`;
+			res.status(200).send(text);
+		})
+		.catch((error) => next(error));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}...`));
